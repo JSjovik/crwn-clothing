88b1ba46684423cdd6d5f58201c67883e8137bdb
JSjovik <jesper.sjovik@gmail.com>
2022-03-24 15:05:20 -0300

    Fail safe
    Fail safe for trying to fix svg logo bug

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import {Outlet, Link} from 'react-router-dom'
+import {Fragment} from 'react';
+import {ReactComponent as Crwnlogo} from '../../assets/crown.svg';
+
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <Crwnlogo className='logo'/>
+                </Link>
+
+                <div className='links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        Shop
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+
+    );
+};
+
+export default Navigation;
